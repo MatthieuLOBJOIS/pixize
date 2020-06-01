acb84e8e025cf2b63fb8cc2470b6163300eed94a
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-06-01 17:46:24 +0200

    Reconfig eslint and prettier

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,23 +1,21 @@
 import React, { useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Home from '../Pages/Home';
-import NotFound from '../Pages/NotFound'
-
-import {
-  Switch,
-  Route,
-} from "react-router-dom";
+import NotFound from '../Pages/NotFound';
 
 const App = ({ getAllStock }) => {
   useEffect(() => {
     getAllStock();
   });
-  return <div className="App">
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  </div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
