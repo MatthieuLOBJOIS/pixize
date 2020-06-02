b70b00123b85fab900a74945496a5a909646cc80
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-06-02 13:08:52 +0200

    add theme semantic-ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
 import store from './store';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-<Provider store={store}>
-  <Router>
-    <App />
-  </Router>
-</Provider>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
