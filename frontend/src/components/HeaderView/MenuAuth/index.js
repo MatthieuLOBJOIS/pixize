02e2f0f4d4a218c51a6e266c1752830f6c770db1
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-06-14 16:57:31 +0200

    Implementation static menu auth

--- frontend/src/components/HeaderView/MenuAuth/index.js
+++ frontend/src/components/HeaderView/MenuAuth/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Menu, Button, Icon } from 'semantic-ui-react';
 
 import useStyles from './style';
 
 const MenuAuth = () => {
 	const classes = useStyles();
+	const [ darkMode, setDarkMode ] = useState(false);
+
+	const handleDarkMode = () => {
+		switch (darkMode) {
+			case false:
+				setDarkMode(true);
+				break;
+			case true:
+				setDarkMode(false);
+				break;
+		}
+	};
+
 	return (
 		<Menu.Menu className={classes.menu}>
-			<Menu.Item position="right">menu d'authentification</Menu.Item>
+			<Menu.Item className={classes.item} position="right">
+				<Button basic color="orange">
+					Connexion
+				</Button>
+
+				<Button basic color="grey">
+					Inscription
+				</Button>
+			</Menu.Item>
+			<Menu.Item>
+				<Icon
+					className={classes.darkMode}
+					size="huge"
+					color={darkMode === false && 'orange'}
+					onClick={handleDarkMode}
+					loading
+					name={darkMode === false ? 'sun' : 'moon'}
+				/>
+			</Menu.Item>
 		</Menu.Menu>
 	);
 };
--- frontend/src/components/HeaderView/MenuAuth/style.js
+++ frontend/src/components/HeaderView/MenuAuth/style.js
@@ -2,7 +2,14 @@ import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
 	menu: {},
-	item: {}
+	item: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between'
+	},
+	darkMode: {
+		cursor: 'pointer'
+	}
 });
 
 export default useStyles;
