355b03c2dd9b3490ac0ba1878aea84c53c5e825c
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-07-04 15:43:52 +0200

    feat(action) log out user

--- frontend/src/actions/user.js
+++ frontend/src/actions/user.js
@@ -3,9 +3,11 @@ export const ON_CHANGE_INPUT = 'ON_CHANGE_INPUT';
 export const ON_CHANGE_CHECK = 'ON_CHANGE_CHECK';
 export const REGISTER_USER = 'REGISTER_USER';
 export const LOGIN_USER = 'LOGIN_USER';
+export const LOGOUT_USER = 'LOGOUT_USER';
 export const IS_SUBMIT = 'IS_SUBMIT';
 export const CREATED_USER = 'CREATED_USER';
 export const CONNECTED_USER = 'CONNECTED_USER';
+export const DISCONNECT_USER = 'DISCONNECT_USER';
 export const CLEAR_FIELD = 'CLEAR_FIELD';
 
 // action creators
@@ -18,7 +20,7 @@ export const CLEAR_FIELD = 'CLEAR_FIELD';
 export const onChangeInput = (data, identifier) => ({
 	type: ON_CHANGE_INPUT,
 	data,
-	identifier
+	identifier,
 });
 
 /**
@@ -29,19 +31,23 @@ export const onChangeInput = (data, identifier) => ({
 export const onChangeCheck = (data, identifier) => ({
 	type: ON_CHANGE_CHECK,
 	data,
-	identifier
+	identifier,
 });
 
 export const registerUser = () => ({
-	type: REGISTER_USER
+	type: REGISTER_USER,
 });
 
 export const loginUser = () => ({
-	type: LOGIN_USER
+	type: LOGIN_USER,
+});
+
+export const logoutUser = () => ({
+	type: LOGOUT_USER,
 });
 
 export const isSubmit = () => ({
-	type: IS_SUBMIT
+	type: IS_SUBMIT,
 });
 
 /**
@@ -52,7 +58,7 @@ export const isSubmit = () => ({
 export const createdUser = (response, message) => ({
 	type: CREATED_USER,
 	response,
-	message
+	message,
 });
 
 /**
@@ -63,9 +69,13 @@ export const createdUser = (response, message) => ({
 export const connectedUser = (response, message) => ({
 	type: CONNECTED_USER,
 	response,
-	message
+	message,
+});
+
+export const disconnectUser = () => ({
+	type: DISCONNECT_USER,
 });
 
 export const clearField = () => ({
-	type: CLEAR_FIELD
+	type: CLEAR_FIELD,
 });
