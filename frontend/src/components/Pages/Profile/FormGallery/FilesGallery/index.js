5496d02cf4795f0d534d0b2f903261fb5bf7f997
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-08-02 22:55:41 +0200

    refactor(FilesGallery): add message if not array of file found

--- frontend/src/components/Pages/Profile/FormGallery/FilesGallery/index.js
+++ frontend/src/components/Pages/Profile/FormGallery/FilesGallery/index.js
@@ -37,45 +37,65 @@ const FilesGallery = ({ stocksCurrentUser }) => {
 		<div className={classes.root}>
 			<p className={classes.titleStock}>Mes photos : </p>
 			<div className={classes.imageBlock}>
-				{imageArray.map((stock) => {
-					return <img className={classes.image} key={stock._id} src={stock.stockUrl} alt={stock.filename} />;
-				})}
+				{imageArray.length !== 0 ? (
+					imageArray.map((stock) => {
+						return (
+							<img className={classes.image} key={stock._id} src={stock.stockUrl} alt={stock.filename} />
+						);
+					})
+				) : (
+					<p>Aucune photo dans la galerie</p>
+				)}
 			</div>
 
 			<p className={classes.titleStock}>Mes Musiques : </p>
 			<div className={classes.audioBlock}>
-				{audioArray.map((stock) => {
-					return (
-						<div className={classes.audioElement} key={stock._id}>
-							<p>{stock.originalname.split('.')[0]}</p>
-							<audio className={classes.audio} controls src={stock.stockUrl}>
-								<track default kind="captions" srcLang="en" />
-								Your browser does not support the
-								<code>audio</code> element.
-							</audio>
-						</div>
-					);
-				})}
+				{audioArray.length !== 0 ? (
+					audioArray.map((stock) => {
+						return (
+							<div className={classes.audioElement} key={stock._id}>
+								<p>{stock.originalname.split('.')[0]}</p>
+								<audio className={classes.audio} controls src={stock.stockUrl}>
+									<track default kind="captions" srcLang="en" />
+									Your browser does not support the
+									<code>audio</code> element.
+								</audio>
+							</div>
+						);
+					})
+				) : (
+					<p>Aucune musique dans la galerie</p>
+				)}
 			</div>
 
 			<p className={classes.titleStock}>Mes vidéos : </p>
 			<div className={classes.videoBlock}>
-				{videoArray.map((stock) => {
-					return (
-						<video className={classes.video} key={stock._id} controls>
-							<source src={stock.stockUrl} type="video/mp4" />
-							<track default kind="captions" srcLang="en" />
-							Sorry, your browser doesn't support embedded videos.
-						</video>
-					);
-				})}
+				{videoArray.length !== 0 ? (
+					videoArray.map((stock) => {
+						return (
+							<video className={classes.video} key={stock._id} controls>
+								<source src={stock.stockUrl} type="video/mp4" />
+								<track default kind="captions" srcLang="en" />
+								Sorry, your browser doesn't support embedded videos.
+							</video>
+						);
+					})
+				) : (
+					<p>Aucune vidéo dans la galerie</p>
+				)}
 			</div>
 
 			<p className={classes.titleStock}>Mes Illustrations : </p>
 			<div className={classes.imageBlock}>
-				{artworkArray.map((stock) => {
-					return <img className={classes.image} key={stock._id} src={stock.stockUrl} alt={stock.filename} />;
-				})}
+				{artworkArray.length !== 0 ? (
+					artworkArray.map((stock) => {
+						return (
+							<img className={classes.image} key={stock._id} src={stock.stockUrl} alt={stock.filename} />
+						);
+					})
+				) : (
+					<p>Aucune illustration dans la galerie</p>
+				)}
 			</div>
 		</div>
 	);
