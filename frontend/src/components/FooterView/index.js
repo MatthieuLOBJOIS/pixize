7c936657c05dde7b3a7d421c9068e005dd516eb2
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-08-19 11:15:24 +0200

    chore(Footer): update style footer and add static link

--- frontend/src/components/FooterView/index.js
+++ frontend/src/components/FooterView/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Segment, Responsive } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 import useStyles from './style';
 
 const FooterView = () => {
 	const classes = useStyles();
 	return (
-		<Responsive className={classes.footer} as={Segment} minWidth={600}>
-			Footer
-		</Responsive>
+		<Segment className={classes.footer}>
+			<div className={classes.linkBlock}>
+				<Link className={classes.link} to="/info">
+					Qui somme nous ?
+				</Link>
+				<Link className={classes.link} to="/mention-legal">
+					Mention Légal
+				</Link>
+				<Link className={classes.link} to="/contact">
+					Contact
+				</Link>
+			</div>
+			<p>&copy; Pixize 2020</p>
+		</Segment>
 	);
 };
 
--- frontend/src/components/FooterView/style.js
+++ frontend/src/components/FooterView/style.js
@@ -2,7 +2,29 @@ import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
 	footer: {
+		backgroundColor: '#564B3F !important',
 		height: '10%',
+		width: '100vw',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+
+		'@media (min-width: 600px)': {},
+		'@media (max-width: 599px)': {
+			paddingBottom: '5em !important',
+		},
+	},
+	linkBlock: {
+		width: '80%',
+		display: 'flex',
+		justifyContent: 'space-around',
+	},
+	link: {
+		color: '#EFC26F !important',
+		'&:hover': {
+			color: 'orange !important',
+			textDecoration: 'underline',
+		},
 	},
 });
 
